4ce79cb32bd4ada4e06e0b54159b1b2a380e7693
mpmicheal <111928941+mpmicheal@users.noreply.github.com>
2024-11-07 01:09:42 -0600

    create alert box

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 function init(){
-//add your javascrip between these two lines of code
+  var button = document.getElementById('entrybutton');
+  
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Micheal Okiror: " + textbox.value);
+  }
+  
+  
+  
  
 
 
